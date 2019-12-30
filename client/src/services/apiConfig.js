99e1c3d4ae66d4480d9d5f61a4a3ff017fbb3fea
Yev <yevyork@gmail.com>
2019-12-30 18:14:10 -0500

    update apiconfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,7 +1,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://sei-items-api.herokuapp.com/api',
+  production: 'https://yeveloper.herokuapp.com/api',
   development: 'http://localhost:3000/api'
 }
 
